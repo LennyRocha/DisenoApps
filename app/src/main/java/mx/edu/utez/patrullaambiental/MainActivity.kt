ea303f0e837b647a065a985a3fbc8d25b5ecad96
Leonardo <20233tn094@utez.edu.mx>
2024-11-11 22:52:43 -0600

    Si

---
+++ app/src/main/java/mx/edu/utez/patrullaambiental/MainActivity.kt
@@ -0,0 +1,44 @@
+package mx.edu.utez.patrullaambiental
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import mx.edu.utez.patrullaambiental.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Simular un retardo
+        Handler().postDelayed({
+            val sharedPreferences = getSharedPreferences("archivo", Context.MODE_PRIVATE)
+            val valor = sharedPreferences.getString("usuario","#")
+
+            if(valor != "#"){
+                // Iniciar la actividad principal después del retardo
+                val intent = Intent(this@MainActivity, Login::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            }else{
+                val intent = Intent(this@MainActivity, Inicio::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                Toast.makeText(this@MainActivity,"Bienvenido "+valor,Toast.LENGTH_SHORT).show()
+            }
+            finish() }, 3000) // 3000 milisegundos = 3 segundos
+    }
+}
