b9da7dc70d82291233b60ebe0b379ffca76a4d29
Leonardo <20233tn094@utez.edu.mx>
2024-11-12 13:39:43 -0600

    Diseño login hecho

--- app/src/main/java/mx/edu/utez/patrullaambiental/Login.kt
+++ app/src/main/java/mx/edu/utez/patrullaambiental/Login.kt
@@ -1,20 +1,39 @@
 package mx.edu.utez.patrullaambiental
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import mx.edu.utez.patrullaambiental.databinding.ActivityLoginBinding
+import java.util.Locale
 
 class Login : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_login)
+
+        val sharedPreferences = getSharedPreferences("archivo", Context.MODE_PRIVATE)
+
+        binding.btnAcceder.setOnClickListener{
+            if(binding.edtUsuario.text.toString().isNotEmpty() && binding.edtContra.text.toString().isNotEmpty()){
+                if(binding.edtUsuario.text.toString().toLowerCase(Locale.ROOT) == "leo" && binding.edtContra.text.toString() == "123"){
+                    with(sharedPreferences.edit()){
+                        putString("usuario",binding.edtUsuario.text.toString())
+                        commit()
+                    }
+                    val intent = Intent(this@Login, Inicio::class.java)
+                    startActivity(intent)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                }else{
+                    Toast.makeText(this@Login,"El usuario y una contraseña no son correctos cuyeyo", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this@Login,"Escribe un usuario y una contraseña", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
