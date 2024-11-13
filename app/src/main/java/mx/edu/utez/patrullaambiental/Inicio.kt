adf8ec2f191744249150a2ecff01ffc4c1cea45e
Leonardo <20233tn094@utez.edu.mx>
2024-11-13 00:18:31 -0600

    Utilizaremos fragments para el menú principal

--- app/src/main/java/mx/edu/utez/patrullaambiental/Inicio.kt
+++ app/src/main/java/mx/edu/utez/patrullaambiental/Inicio.kt
@@ -1,18 +1,18 @@
 package mx.edu.utez.patrullaambiental
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import mx.edu.utez.patrullaambiental.databinding.ActivityInicioBinding
 
 class Inicio : AppCompatActivity() {
+    private val fragment1 = Frag_inicio()
+    private val fragment2 = Frag_mapa()
     private lateinit var binding : ActivityInicioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +23,36 @@ class Inicio : AppCompatActivity() {
 
         val sharedPreferences = getSharedPreferences("archivo", Context.MODE_PRIVATE)
         val valor = sharedPreferences.getString("usuario","#")
-        binding.txtBienvenido.setText(resources.getString(R.string.bienve)+" "+valor)
 
+        // Configuramos el listener para el BottomNavigationView
+        binding.myToolbar2.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item1 -> {
+                    loadFragment(fragment1)
+                    true
+                }
+                R.id.item2 -> {
+                    loadFragment(fragment2)
+                    true
+                }
+                /*R.id.item3 -> {
+                    loadFragment(thirdFragment)
+                    true
+                }*/
+                else -> false
+            }
+        }
 
+        // Cargar el primer fragmento por defecto
+        loadFragment(fragment1)
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(binding.contenedor.id, fragment)
+        transaction.commit()
+    }
+
 
     //Asigne el menú a la activity (se va a ver)
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -49,7 +75,9 @@ class Inicio : AppCompatActivity() {
                         remove("usuario")
                         commit()
                     }
-                    finish()
+                    val intent = Intent(this@Inicio,MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
                 }
                 builder.setNegativeButton("No") { dialog, _ ->
                     dialog.cancel()
