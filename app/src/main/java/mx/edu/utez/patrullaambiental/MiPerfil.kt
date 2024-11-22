0d26efc60f5ca0fe3a7320b33618ee2239b76a4a
Leonardo <20233tn094@utez.edu.mx>
2024-11-22 11:38:24 -0600

    Activity mi perfil listo

--- app/src/main/java/mx/edu/utez/patrullaambiental/MiPerfil.kt
+++ app/src/main/java/mx/edu/utez/patrullaambiental/MiPerfil.kt
@@ -1,10 +1,22 @@
 package mx.edu.utez.patrullaambiental
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.Rect
+import android.graphics.RectF
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +28,7 @@ import mx.edu.utez.patrullaambiental.databinding.ActivityMiPerfilBinding
 
 class MiPerfil : AppCompatActivity() {
     private lateinit var binding : ActivityMiPerfilBinding
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMiPerfilBinding.inflate(layoutInflater)
@@ -24,8 +37,37 @@ class MiPerfil : AppCompatActivity() {
         setSupportActionBar(binding.myToolbar2)
 
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        binding.imgPerfil.apply {
+            layoutParams = LinearLayout.LayoutParams( LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT )
+            scaleType = ImageView.ScaleType.FIT_CENTER
+        }
+
+        binding.imgPerfil.setImageBitmap(getRoundedCornerBitmap(getResources().getDrawable(R.drawable.perfil_default), 2f));
     }
 
+    fun getRoundedCornerBitmap(drawable: Drawable, roundPx: Float): Bitmap {
+        val bitmap = (drawable as BitmapDrawable).bitmap
+        val width = bitmap.width
+        val height = bitmap.height
+
+        val output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(output)
+        val color = 0xff424242.toInt()
+        val paint = Paint()
+        val rect = Rect(0, 0, width, height)
+        val rectF = RectF(rect)
+
+        paint.isAntiAlias = true
+        canvas.drawARGB(0, 0, 0, 0)
+        paint.color = color
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint)
+
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawBitmap(bitmap, rect, rect, paint)
+
+        return output
+    }
 
     //Asigne el menú a la activity (se va a ver)
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
