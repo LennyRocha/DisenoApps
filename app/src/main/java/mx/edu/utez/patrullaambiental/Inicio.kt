9156a25f7474358856e2b3c198a6d6389e5d8520
Leonardo <20233tn094@utez.edu.mx>
2024-11-29 09:10:07 -0600

    Logo de la aplicación terminado, y cantidad de items del menú inferior reducido a 3

--- app/src/main/java/mx/edu/utez/patrullaambiental/Inicio.kt
+++ app/src/main/java/mx/edu/utez/patrullaambiental/Inicio.kt
@@ -5,15 +5,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Space
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import mx.edu.utez.patrullaambiental.databinding.ActivityInicioBinding
 
 class Inicio : AppCompatActivity() {
     private val fragment1 = Frag_inicio()
-    private val fragment2 = Frag_correo()
+    private val fragment2 = Frag_noticias()
     private val fragment3 = Frag_mapa()
+    private val fragment4 = Test_frag()
     private lateinit var binding : ActivityInicioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,14 @@ class Inicio : AppCompatActivity() {
 
         setSupportActionBar(binding.myToolbar)
 
-        binding.myToolbar.setLogo(R.mipmap.ic_launcher_round)
+        binding.myToolbar.setLogo(R.drawable.logito)
+        val space = Space(this).apply {
+            layoutParams = Toolbar.LayoutParams(32, Toolbar.LayoutParams.WRAP_CONTENT)
+        }
+        binding.myToolbar.addView(space, 2)
+        binding.myToolbar.setTitle(". "+getString(R.string.app_name_spaced))
+
+        //supportActionBar?.setDisplayShowTitleEnabled(false)
 
         val sharedPreferences = getSharedPreferences("archivo", Context.MODE_PRIVATE)
         val valor = sharedPreferences.getString("usuario","#")
@@ -34,11 +44,11 @@ class Inicio : AppCompatActivity() {
                     loadFragment(fragment1)
                     true
                 }
-                R.id.item2 -> {
+                R.id.item3 -> {
                     loadFragment(fragment2)
                     true
                 }
-                R.id.item3 -> {
+                R.id.item2 -> {
                     loadFragment(fragment3)
                     true
                 }
@@ -91,6 +101,9 @@ class Inicio : AppCompatActivity() {
                 val intent = Intent(this@Inicio,MiPerfil::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                 startActivity(intent)
+            }
+            R.id.config -> {
+                loadFragment(fragment4)
             }
         }
         return super.onOptionsItemSelected(item)
