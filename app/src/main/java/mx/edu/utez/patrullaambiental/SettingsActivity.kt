420470d0c24de4a9953268420cd88e57f3b42f22
LennyRocha <20233tn094@utez.edu.mx>
2024-12-09 23:01:51 -0600

    Avance de activity de settings,

--- app/src/main/java/mx/edu/utez/patrullaambiental/SettingsActivity.kt
+++ app/src/main/java/mx/edu/utez/patrullaambiental/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package mx.edu.utez.patrullaambiental
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,5 +15,27 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar2)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24_on_back)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                // Manejar la acción de la flecha aquí
+                val intent = Intent(this@SettingsActivity,Inicio::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
